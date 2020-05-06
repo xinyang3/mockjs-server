3193e2c85b2dc06942f16fee2ea040c4074102d3
bijiaju <247636225@qq.com>
2020-05-06 13:58:48 +0800

    跨域处理

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ app.use(morgan('short'));
 
 app.use(router)
 
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization,Origin,Accept,X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.listen(config.port, config.host, () => {
   console.debug('mockjs server is running at \' http:127.0.0.1://3000 \' !')
 });
